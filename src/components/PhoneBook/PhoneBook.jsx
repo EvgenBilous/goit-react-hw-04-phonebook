dd824afeb0838e264ba3453a50641bcb993d148e
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-27 19:48:04 -0600

    refactoring

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -1,46 +1,49 @@
-import { Component } from 'react';
-export default class PhoneBook extends Component {
-  state = {
-    name: '',
-    number: '',
+import { React, useState } from 'react';
+
+const PhoneBook = ({ onSubmit }) => {
+  const [state, setState] = useState({ name: '', number: '' });
+
+  const handleChangeInputsForm = event => {
+    const { name, value } = event.target;
+    setState({ ...state, [name]: value });
   };
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit(state);
+    setState({ name: '', number: '' });
   };
-  handleSubmit = evt => {
-    evt.preventDefault();
-    this.props.addContact(this.state);
-    this.setState({ name: '', number: '' });
-  };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            onChange={this.handleChange}
-            type="text"
-            name="name"
-            minLength={2}
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
-            required
-            value={this.state.name}
-          />
-        </label>
-        <label>
-          Number
-          <input
-            onChange={this.handleChange}
-            type="tel"
-            name="number"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-          />
-        </label>
-        <button type="submit">Add Contact</button>
-      </form>
-    );
-  }
-}
+
+  const { name, number } = state;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          onChange={handleChangeInputsForm}
+          type="text"
+          name="name"
+          minLength={2}
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+          required
+          value={name}
+        />
+      </label>
+      <label>
+        Number
+        <input
+          onChange={handleChangeInputsForm}
+          type="tel"
+          name="number"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default PhoneBook;
