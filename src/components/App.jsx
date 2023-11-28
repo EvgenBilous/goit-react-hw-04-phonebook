b0ce5463496ce9aff2a8bee496cbbff3fc499f47
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-28 10:56:06 -0600

    refactoring session2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import PhoneBook from './PhoneBook/PhoneBook';
 import Section from './Section/Section';
 import ContactsList from './ContactsList/ContactsList';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import SearchFilter from './SearchFilter/SearchFilter';
 
@@ -12,83 +12,154 @@ const initialContacts = [
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-export default class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+// export default class App extends Component {
+//   // state = {
+//   //   contacts: [],
+//   //   filter: '',
+//   // };
 
-  componentDidMount = () => {
+//   // componentDidMount = () => {
+//   //   const storage = localStorage.getItem('contacts');
+//   //   if (!storage) {
+//   //     this.setState({ contacts: initialContacts });
+//   //     return;
+//   //   }
+//   //   const parsedStorage = JSON.parse(storage);
+//   //   this.setState({ contacts: parsedStorage });
+//   // };
+
+//   // componentDidUpdate = (_, prevState) => {
+//   //   const { contacts } = this.state;
+//   //   if (prevState.contacts !== contacts) {
+//   //     localStorage.setItem('contacts', JSON.stringify(contacts));
+//   //   }
+//   // };
+
+//   handleFilterContacts = () => {
+//     const { contacts, filter } = this.state;
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+//   };
+
+//   handleFilter = evt => {
+//     const { value } = evt.target;
+//     this.setState({ filter: value });
+//   };
+
+//   addContact = newContact => {
+//     const { contacts } = this.state;
+//     const isDuplicateContact = contacts.some(
+//       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//     );
+//     if (isDuplicateContact) {
+//       alert(`${newContact.name} is already in contacts `);
+//       return;
+//     }
+
+//     this.setState(prevState => {
+//       return {
+//         contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+//       };
+//     });
+//   };
+//   deleteContact = id => {
+//     this.setState(prevState => {
+//       return {
+//         contacts: prevState.contacts.filter(contact => contact.id !== id),
+//       };
+//     });
+//   };
+
+//   render() {
+//     const filteredContacts = this.handleFilterContacts();
+//     return (
+//       <div>
+//         <Section title={'PhoneBook'}>
+//           <PhoneBook addContact={onsubmit} />
+//         </Section>
+//         <Section title={'Contacts'}>
+//           <SearchFilter
+//             filter={this.state.filter}
+//             handleFilter={this.handleFilter}
+//           />
+//           <ContactsList
+//             contacts={filteredContacts}
+//             deleteContact={this.deleteContact}
+//           />
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(initialContacts);
+
+  useEffect(() => {
     const storage = localStorage.getItem('contacts');
     if (!storage) {
-      this.setState({ contacts: initialContacts });
+      setContacts(initialContacts);
       return;
     }
     const parsedStorage = JSON.parse(storage);
-    this.setState({ contacts: parsedStorage });
-  };
+    setContacts(parsedStorage);
+  }, []);
 
-  componentDidUpdate = (_, prevState) => {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
+  useEffect(() => {
+    const previousContacts = JSON.stringify(contacts);
+    const storage = localStorage.getItem('contacts');
+    if (!storage || JSON.stringify(storage) !== previousContacts) {
+      localStorage.setItem('contacts', previousContacts);
     }
-  };
+  }, [contacts]);
 
-  handleFilterContacts = () => {
-    const { contacts, filter } = this.state;
+  const handleFilterContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  handleFilter = evt => {
-    const { value } = evt.target;
-    this.setState({ filter: value });
+  const handleFilter = evt => {
+    setFilter(evt.currentTarget.value);
   };
 
-  addContact = newContact => {
-    const { contacts } = this.state;
+  const addContact = (name, number) => {
+    //const idShortid = shortid.generate();
     const isDuplicateContact = contacts.some(
-      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      contact => contact.name.toLowerCase() === name.toLowerCase()
     );
     if (isDuplicateContact) {
-      alert(`${newContact.name} is already in contacts `);
+      alert(`${name} is already in contacts `);
       return;
     }
-
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
-      };
-    });
+    const newContact = {
+      name,
+      number,
+    };
+    setContacts(prevContacts => [...prevContacts, { ...newContact }]);
   };
-  deleteContact = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
-  };
-
-  render() {
-    const filteredContacts = this.handleFilterContacts();
-    return (
-      <div>
-        <Section title={'PhoneBook'}>
-          <PhoneBook addContact={this.addContact} />
-        </Section>
-        <Section title={'Contacts'}>
-          <SearchFilter
-            filter={this.state.filter}
-            handleFilter={this.handleFilter}
-          />
-          <ContactsList
-            contacts={filteredContacts}
-            deleteContact={this.deleteContact}
-          />
-        </Section>
-      </div>
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
-  }
-}
+  };
+
+  return (
+    <div>
+      <Section title={'PhoneBook'}>
+        <PhoneBook addContact={addContact} />
+      </Section>
+      <Section title={'Contacts'}>
+        <SearchFilter filter={filter} onChange={handleFilter} />
+        <ContactsList
+          contacts={handleFilterContacts}
+          deleteContact={deleteContact}
+        />
+      </Section>
+    </div>
+  );
+};
+
+export default App;
